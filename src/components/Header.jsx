1c0fc658169fa1ea5e6a8f57c6e5ca09f44fcdf3
Olayinka O. <omosehintemilade@gmail.com>
2024-11-01 16:44:00 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <Header>
+                <h1>Da-ala Quincy</h1>
+
+                <div className="navigation">
+                    <nav>HOME</nav>
+                    <nav>ABOUT</nav>
+                    <nav>SKILLS</nav>
+                    <nav>CONTACT</nav>
+                </div>
+    </Header>
+  )
+}
+
+export default Header
